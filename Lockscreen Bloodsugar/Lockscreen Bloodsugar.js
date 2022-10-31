d377efdb8c9b6619a6d7c63e5156db27afd04a5e
TheGoodDude22 <61601241+TheGoodDude22@users.noreply.github.com>
2022-10-31 11:15:16 -0500

    Update Lockscreen Bloodsugar.js

--- Lockscreen Bloodsugar/Lockscreen Bloodsugar.js
+++ Lockscreen Bloodsugar/Lockscreen Bloodsugar.js
@@ -1,7 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-purple; icon-glyph: vial;
-
 // Requires Sugarmate app
 // Set up the Sugarmate apple watch calendar. It must be named "Sugarmate Readings".
 
@@ -10,7 +9,8 @@ const widget = await createWidget()
 if (config.runsInWidget) {
   Script.setWidget(widget)
 } else {
-  widget.presentAccessoryInline()
+//   widget.presentAccessoryInline()
+  widget.presentAccessoryRectangular()
 }
 
 async function createWidget() {
@@ -55,11 +55,14 @@ async function createWidget() {
         
         if (readingNumber) {
           let numberOut = output.addText(readingNumber)
-          numberOut.font = Font.boldSystemFont(20)
+          numberOut.font = Font.boldSystemFont(22)
+          numberOut.minimumScaleFactor = 0.5
           numberOut.centerAlignText()
         }
         let infoOut = output.addText(readingInfo)
         infoOut.centerAlignText()
+        infoOut.lineLimit = 1
+        infoOut.minimumScaleFactor = 0.6
         
       } else {
         
@@ -76,10 +79,16 @@ async function createWidget() {
         
         const stack = output.addStack()
       stack.centerAlignContent()
-        const outNumber = stack.addText(readingNumber)
+        const outNumber = stack.addText(readingNumber + "  ")
         outNumber.font = Font.boldSystemFont(40)
-        const outInfo = stack.addText(" " + readingInfo)
+        outNumber.minimumScaleFactor = 0.75
+        // stack.addSpacer(10)
+        // stack.addSpacer()
+        const outInfo = stack.addText(readingInfo)
         outInfo.font = Font.systemFont(26)
+        outInfo.lineLimit = 2
+        outInfo.minimumScaleFactor = 0.5
+        outInfo.centerAlignText()
         
       } else {
         
@@ -105,6 +114,10 @@ async function getReading() {
   const readings = await CalendarEvent.between(oldTime, newTime, [smCal]);
 
   return readings[readings.length - 1]?.title
+  // Test cases
+  // return "204 ↑↑ +17  (mg/dL)"
+  // return "282 → [OLD] (mg/dL)"
+  // return "122 ➚ +24  (mg/dL)"
 }
 
 function addMinutes(date, minutes) {
